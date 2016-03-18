809dc8c2a2fc9047221997d977b98c8d4916c601
dmitri-desh <dmitri.desh@gmail.com>
2016-03-18 16:06:55 +0300

    Added files via upload
    Upload "Orders" project

---
+++ Orders/Orders/OrderRow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Orders
+{
+    public class OrderRow 
+    {
+        public int RowID { get; set; }
+        public string RoomType { get; set; }
+        public decimal RoomPrice { get; set; }
+        public string ConsmrName { get; set; }
+        public OrderRow (int rowID, string roomType, decimal roomPrice, string consmrName)
+        {
+            this.RowID = rowID;
+            this.RoomType = roomType;
+            this.ConsmrName = consmrName;
+        }
+    }
+}
---
+++ Orders/Orders/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OrderRow oRow = new OrderRow(1, "Room Type 1", 100, "Pupkin V.V.");
+            Order o = new Order(1, "JSC Horns & Hooves", DateTime.Now, oRow);
+            o.PrintDoc(1);  // Print Invoice!
+        }
+    }
+}
