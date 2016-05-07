c478e0b891410e6eafc555b518bd206bf4a03906
dmitri-desh <dmitri.desh@gmail.com>
2016-05-07 21:56:26 +0300

    commit changes

---
+++ Task3/Billing/AccountRefill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Billing
+{
+    public class AccountRefill
+    {
+        public DateTime Date { get; protected set; }
+        public decimal Amount { get; protected set; }
+        public AccountRefill(decimal amount)
+        {
+            this.Date = DateTime.Now;
+            this.Amount = amount;
+        }
+    }
+}
