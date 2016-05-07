5a69c9f2c4a46be42c6684e201c54d59e5392303
dmitri-desh <dmitri.desh@gmail.com>
2016-05-07 20:43:44 +0300

    Account.cs added

---
+++ Task3/Billing/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Billing
+{
+    public class Account
+    {
+        public Customer Customer { get; protected set; }
+        public Contract Contract { get; protected set; }
+        public List<CallInfoFull> CallsHistory = new List<CallInfoFull>();
+        public List<BillingPlan> BillingPlanHistory = new List<BillingPlan>();
+    }
+}
