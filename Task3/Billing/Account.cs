8acce575d2a107931c81b8a3ffe42b2064041a79
dmitri-desh <dmitri.desh@gmail.com>
2016-05-10 21:12:22 +0300

    commit Account.cs

--- Task3/Billing/Account.cs
+++ Task3/Billing/Account.cs
@@ -10,10 +10,10 @@ namespace Billing
         public int AccountId { get; protected set; }
         public Customer Customer { get; protected set; }
         
-        public ICollection<Contract> Contracts { get; protected set; }
+        public ICollection<Contract> Contracts { get; set; }
         public decimal Amount { get; protected set; }
 
-        public ICollection<AccountRefill> AccountRefillLog { get; protected set; }
+        public ICollection<AccountRefill> AccountRefillLog { get; set; }
         public Account(int accountId, Customer customer)
         {
             this.AccountId = accountId;
@@ -31,6 +31,11 @@ namespace Billing
             if (amount > 0)
             this.Amount += amount;
             this.AccountRefillLog.Add(new AccountRefill(amount));
+        }
+        public override string ToString()
+        {
+            return this.Customer.Name +" Баланс: "+ this.Amount.ToString();
+                //base.ToString();
         }
     }
 }
