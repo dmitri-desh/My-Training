e13ecc5bb695e8fe17a0c30b0ceb926b6e38d52a
dmitri-desh <dmitri.desh@gmail.com>
2016-05-10 21:15:51 +0300

    commit

---
+++ Task3/Billing/BillingDataBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Billing
+{
+    public class BillingDataBase 
+    {
+        public ICollection<Account> Accounts { get; set; }
+        public BillingDataBase()
+        {
+            Accounts = new List<Account>();
+        }
+    }
+}
