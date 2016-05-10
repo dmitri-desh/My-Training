ed70299be19412359dbe3c21e310008ce4298a2d
dmitri-desh <dmitri.desh@gmail.com>
2016-05-10 21:16:09 +0300

    commit

--- Task3/Builder/Builder.cs
+++ Task3/Builder/Builder.cs
@@ -9,7 +9,7 @@ namespace Builder
     abstract class Builder
     {
         public abstract void Initialize();
-        public abstract void ReadFile();
+        public abstract void Emulate();
         public abstract void GetResult();
 
     }
