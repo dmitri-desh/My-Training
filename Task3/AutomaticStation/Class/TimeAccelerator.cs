c43b2740a1baaeffee85ce431d37493b58b74899
dmitri-desh <dmitri.desh@gmail.com>
2016-05-16 12:44:57 +0300

    App.config disabled

--- Task3/AutomaticStation/Class/TimeAccelerator.cs
+++ Task3/AutomaticStation/Class/TimeAccelerator.cs
@@ -11,17 +11,18 @@ namespace AutomaticStation
        
         public static TimeSpan AccelerateTime(DateTime start) // emulate the acceleration time "times" times
         {
-            int times;
+            int times = 10000;
             TimeSpan span = new TimeSpan();
-            Int32.TryParse(ConfigurationManager.AppSettings["times"], out times);
+           // Int32.TryParse(ConfigurationManager.AppSettings["times"], out times);
             if (start != null)
             {
                span = DateTime.Now.Subtract(start);
                var now = start.AddSeconds(span.TotalSeconds * times);
                 span = now - start;
                 Console.WriteLine("Duration Accelerated {1} times: {0}", span.ToString(), times);
-             //   return span;
+                return span;
             }
+           
             return span;
         }
     }
