af18183d66b1eafb814f9c1cd06340aaf1bb10a9
dmitri-desh <dmitri.desh@gmail.com>
2016-05-17 16:36:29 +0300

    Init Task4

---
+++ Task4/Model/Model.edmx.sql
@@ -0,0 +1,162 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 05/17/2016 15:55:10
+-- Generated from EDMX file: S:\Dropbox\Users\14010\Documents\Visual Studio 2015\Projects\My-Training\Task4\Model\Model.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [tempdb];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[FK_ManagerOrder]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[OrderSet] DROP CONSTRAINT [FK_ManagerOrder];
+GO
+IF OBJECT_ID(N'[dbo].[FK_CustomerOrder]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[OrderSet] DROP CONSTRAINT [FK_CustomerOrder];
+GO
+IF OBJECT_ID(N'[dbo].[FK_ProductOrder]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[OrderSet] DROP CONSTRAINT [FK_ProductOrder];
+GO
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[ManagerSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[ManagerSet];
+GO
+IF OBJECT_ID(N'[dbo].[CustomerSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[CustomerSet];
+GO
+IF OBJECT_ID(N'[dbo].[ProductSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[ProductSet];
+GO
+IF OBJECT_ID(N'[dbo].[OrderSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[OrderSet];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'ManagerSet'
+CREATE TABLE [dbo].[ManagerSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [SecondName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'CustomerSet'
+CREATE TABLE [dbo].[CustomerSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [SecondName] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'ProductSet'
+CREATE TABLE [dbo].[ProductSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Name] nvarchar(max)  NOT NULL
+);
+GO
+
+-- Creating table 'OrderSet'
+CREATE TABLE [dbo].[OrderSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [PurchaseDate] datetime  NOT NULL,
+    [Amount] decimal(18,0)  NOT NULL,
+    [ManagerId] int  NOT NULL,
+    [CustomerId] int  NOT NULL,
+    [ProductId] int  NOT NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [Id] in table 'ManagerSet'
+ALTER TABLE [dbo].[ManagerSet]
+ADD CONSTRAINT [PK_ManagerSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'CustomerSet'
+ALTER TABLE [dbo].[CustomerSet]
+ADD CONSTRAINT [PK_CustomerSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'ProductSet'
+ALTER TABLE [dbo].[ProductSet]
+ADD CONSTRAINT [PK_ProductSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- Creating primary key on [Id] in table 'OrderSet'
+ALTER TABLE [dbo].[OrderSet]
+ADD CONSTRAINT [PK_OrderSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- Creating foreign key on [ManagerId] in table 'OrderSet'
+ALTER TABLE [dbo].[OrderSet]
+ADD CONSTRAINT [FK_ManagerOrder]
+    FOREIGN KEY ([ManagerId])
+    REFERENCES [dbo].[ManagerSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ManagerOrder'
+CREATE INDEX [IX_FK_ManagerOrder]
+ON [dbo].[OrderSet]
+    ([ManagerId]);
+GO
+
+-- Creating foreign key on [CustomerId] in table 'OrderSet'
+ALTER TABLE [dbo].[OrderSet]
+ADD CONSTRAINT [FK_CustomerOrder]
+    FOREIGN KEY ([CustomerId])
+    REFERENCES [dbo].[CustomerSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_CustomerOrder'
+CREATE INDEX [IX_FK_CustomerOrder]
+ON [dbo].[OrderSet]
+    ([CustomerId]);
+GO
+
+-- Creating foreign key on [ProductId] in table 'OrderSet'
+ALTER TABLE [dbo].[OrderSet]
+ADD CONSTRAINT [FK_ProductOrder]
+    FOREIGN KEY ([ProductId])
+    REFERENCES [dbo].[ProductSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_ProductOrder'
+CREATE INDEX [IX_FK_ProductOrder]
+ON [dbo].[OrderSet]
+    ([ProductId]);
+GO
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
