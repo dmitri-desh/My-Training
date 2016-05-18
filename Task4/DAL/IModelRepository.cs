b569ebf73e2a7608667f6ee2f215d4abeff34390
dmitri-desh <dmitri.desh@gmail.com>
2016-05-18 16:28:23 +0300

    IModelRepository added

---
+++ Task4/DAL/IModelRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public interface IModelRepository<T>
+    {
+        void Add(T item);
+        void Remove(T item);
+        void Update(T item);
+        IEnumerable<T> Items { get; }
+        void SaveChanges();
+    }
+}
