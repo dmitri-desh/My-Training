e445276f2077e2b0e557f4537e904d5e6c6990fb
dmitri-desh <dmitri.desh@gmail.com>
2016-05-26 08:26:41 +0300

    commit

--- Task4/BL/AppManager.cs
+++ Task4/BL/AppManager.cs
@@ -1,25 +1,95 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BL
 {
     public class AppManager : IDisposable
     {
-        private System.IO.FileSystemWatcher _watcher;
-        public AppManager(System.IO.FileSystemWatcher watcher)
+        private FileSystemWatcher _watcher;
+        private BlockingCollection<CancellationToken> cancelTokenCollection = new BlockingCollection<CancellationToken>();
+        public AppManager(FileSystemWatcher watcher)
         {
             _watcher = watcher;
             _watcher.Created += this.OnWatcherCreated;
         }
 
-        public void OnWatcherCreated(object sender, System.IO.FileSystemEventArgs e)
+        public void OnWatcherCreated(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                CancellationToken token = new CancellationToken();
+
+                Action createTaskAction = () =>
+                {
+                    using (var stream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.None))
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            ParseCsv(reader, token);
+                        }
+                    }
+                };
+
+                var task = new Task(createTaskAction, token);
+                cancelTokenCollection.Add(token);
+
+                task.ContinueWith((sourceTask) =>
+                {
+                    cancelTokenCollection.Take(token);
+                });
+
+                task.Start();
+
+            }
+            finally
+            {
+                // Sql Server 2008 R2
+            }
+        }
+        protected void ParseCsv(TextReader reader, CancellationToken token)
         {
 
-        }
+            try
+            {
+                string current = null;
+                while ((current = reader.ReadLine()) != null && !token.IsCancellationRequested)
+                {
+                    // Parse string
+                    // add records to data context
 
+                    // add Order
+
+
+                }
+
+                if (current != null && token.IsCancellationRequested)
+                {
+                    // Rollback
+                }
+            }
+            catch (IOException e)
+            {
+                //Rollback
+            }
+
+
+        }
+        private object orderSyncObj = new object();
+
+        public void Add(Order order)
+        {
+            // create repository
+            lock (orderSyncObj)
+            {
+
+            }
+        }
         public void Run()
         {
             if (_watcher != null && !_watcher.EnableRaisingEvents)
