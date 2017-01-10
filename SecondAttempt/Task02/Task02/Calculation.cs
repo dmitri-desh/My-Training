6a59f7b0bab2f6755f73d4bbd8221a92d48aa4db
dmitri-desh <dmitri.desh@gmail.com>
2017-01-10 15:04:18 +0300

    commit

---
+++ SecondAttempt/Task02/Task02/Calculation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Task02
+{
+   static class Calculation
+    {
+        static int GetFibonacci(int i)
+        {
+            if (i <= 1) return i;
+            else return GetFibonacci(i - 2) + GetFibonacci(i - 1);
+        }
+
+        static int GetFactorial(int i)
+        {
+            if (i == 1) return i;
+            else return GetFactorial(i - 1) * i;
+        }
+    }
+}
--- SecondAttempt/Task02/Task02/Program.cs
+++ SecondAttempt/Task02/Task02/Program.cs
@@ -10,6 +10,18 @@ namespace Task02
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите число: ");
+            try
+            {
+                int i = int.Parse(Console.ReadLine());
+                Console.WriteLine("{0}-ое число Фибоначчи = {1}", i, Task02.Calculation.GetFibonacci(i));
+                Console.WriteLine("{0}! = {1}", i, Calculation.GetFactorial(i));
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Некорректное число");
+            }
+
         }
     }
 }
