108e9767362cf154c629a6643c99ef5d9856870c
dmitri-desh <dmitri.desh@gmail.com>
2017-01-11 16:33:39 +0300

    commit

---
+++ SecondAttempt/Task02/Task02/ObjCounter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02
+{
+    class ObjCounter : IDisposable
+    {
+        static int count = 0;
+        public ObjCounter ()
+        {
+            count++;
+        }
+       
+        public void Dispose()
+        {
+            count--;
+        }
+
+        public int GetObjectsCount ()
+        {
+            return count;
+        }
+    }
+}
--- SecondAttempt/Task02/Task02/Program.cs
+++ SecondAttempt/Task02/Task02/Program.cs
@@ -14,10 +14,21 @@ namespace Task02
             Console.WriteLine("Введите число: ");
             try
             {
-                
+                // Task02 Calculation
                 int i = int.Parse(Console.ReadLine());
                 Console.WriteLine("{0}-ое число Фибоначчи = {1}", i, Calculation.GetFibonacci(i));
                 Console.WriteLine("{0}! = {1}", i, Calculation.GetFactorial(i));
+
+                // Task02 ObjCounter
+                ObjCounter obj1 = new ObjCounter();
+                ObjCounter obj2 = new ObjCounter();
+                ObjCounter obj3 = new ObjCounter();
+                ObjCounter obj4 = new ObjCounter();
+                ObjCounter obj5 = new ObjCounter();
+                ObjCounter obj6 = new ObjCounter();
+                obj1.Dispose();
+                obj3.Dispose();
+                Console.WriteLine("Образовано {0} объектов класса", obj1.GetObjectsCount());
             }
             catch (FormatException)
             {
