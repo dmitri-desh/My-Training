4d40a667ce272171b9c0b99f788b32f991fa9977
dmitri-desh <dmitri.desh@gmail.com>
2017-01-21 14:21:16 +0300

    Keys classes added

---
+++ SecondAttempt/Task02/Task02/Keys/CommonKey.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task02.Keys
+{
+    abstract class CommonKey
+    {
+        public string Name { get; set; }
+        public int KeyType { get; set; }
+    }
+}
