8b9d450f8b3608b15c7f553a82113c5d90b3a17d
dmitri-desh <dmitri.desh@gmail.com>
2017-03-23 15:27:05 +0300

    Anit  Application

---
+++ forAzot/XMLApplication/XMLApplication/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace XMLApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "out-*.xml";
+            this.openFileDialog1.Filter = " XML-files (out-*.xml)|out-*.xml|All files (*.*)|*.*";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(550, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(610, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Источник XML (out-*.xml)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(610, 61);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Источник XML сторонний (edecl*.xml)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(60, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(550, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "edecl-*.xml";
+            this.openFileDialog2.Filter = " XML-files (edecl*.xml)|edecl*.xml|All files (*.*)|*.*";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(190, 136);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(144, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Обработать...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(394, 136);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(144, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Выход";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(111, 194);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(864, 510);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 716);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ forAzot/XMLApplication/XMLApplication/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.XPath;
+
+namespace XMLApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // openFileDialog1.ShowDialog();
+            openFileDialog1.FileName = "N:\\MARKET\\ORACLE\\DeclRegister\\xsl-edecl\\3\\out-OPS$14010-17-03-10-31.xml";
+            textBox1.Text = openFileDialog1.FileName;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //  openFileDialog2.ShowDialog();
+            openFileDialog2.FileName = "N:\\MARKET\\ORACLE\\DeclRegister\\xsl-edecl\\3\\edeclSNG-201702.xml";
+            textBox2.Text = openFileDialog1.FileName;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            XmlDocument docAzot = new XmlDocument();
+            docAzot.Load(openFileDialog1.FileName);
+            XPathNavigator navigatorAzot = docAzot.CreateNavigator();
+
+           // XmlNamespaceManager managerAzot = new XmlNamespaceManager(navigatorAzot.NameTable);
+           // managerAzot.AddNamespace("bk", "");
+            richTextBox1.Text = navigatorAzot.OuterXml.ToString();
+
+            /*
+            foreach (XPathNavigator navAzot in navigatorAzot.Select("//bk:NDS_v3_part1_t001", managerAzot))
+            {
+                label1.Text = navAzot.OuterXml;
+                Console.WriteLine(navigatorAzot.OuterXml);
+                 if (navAzot.Value == "63465184.84")
+                 {
+                     navAzot.SetValue("12.99");
+
+                
+            }
+            
+            Console.WriteLine(navigatorAzot.OuterXml);
+            */
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
