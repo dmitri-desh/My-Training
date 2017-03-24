e73313eb311eb57dc6ab6ead182ad2d5ee37b3b6
dmitri-desh <dmitri.desh@gmail.com>
2017-03-24 14:32:47 +0300

    commit

--- forAzot/XMLApplication/XMLApplication/Form1.Designer.cs
+++ forAzot/XMLApplication/XMLApplication/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // openFileDialog1
@@ -118,15 +119,25 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(46, 726);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 7;
-            this.label1.Text = "label1";
+            this.label1.Text = "Ошибки:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Read Value...";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1014, 744);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.button4);
@@ -154,6 +165,7 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- forAzot/XMLApplication/XMLApplication/Form1.cs
+++ forAzot/XMLApplication/XMLApplication/Form1.cs
@@ -31,41 +31,116 @@ namespace XMLApplication
 
         private void button2_Click(object sender, EventArgs e)
         {
-            //  openFileDialog2.ShowDialog();
-            openFileDialog2.FileName = "S:\\Visual Studio 2015\\Projects\\My-Training\\forAzot\\XMLApplication\\testXML\\out-OPS$14010-17-03-10-31.xml";
-            textBox2.Text = openFileDialog1.FileName;
+           //   openFileDialog2.ShowDialog();
+            openFileDialog2.FileName = "S:\\Visual Studio 2015\\Projects\\My-Training\\forAzot\\XMLApplication\\testXML\\reestrCBX-201601.xml";
+            textBox2.Text = openFileDialog2.FileName;
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             StreamReader reader = null;
+            StreamReader reader2 = null;
             try
             {
                 reader = File.OpenText(openFileDialog1.FileName);
-                int bufferLength = 10000;
 
+                int bufferLength = 30000;
                 string pattern = @"\s+";
                 string target = " ";
                 Regex regex = new Regex(pattern);
-                             
+
+               // var startTag1 = "<NDS_v3_reestr_t001>";
+               // var endTag1 = "</NDS_v3_reestr_t001>";
+
+                var startTag2 = "<NDS_v3_reestr2_t001>";
+                var endTag2 = "</NDS_v3_reestr2_t001>";
+                var startTag8 = "<NDS_v3_reestr8_t001>";
+                var endTag8 = "</NDS_v3_reestr8_t001>";
+                var flag2 = false;
+                var flag8 = false;
+
+
                 StringBuilder buffer = new StringBuilder(bufferLength);
                 buffer.Clear();
-                string currentString = reader.ReadLine();
-                var prevString = "";
+                string currentString = reader.ReadLine().Trim();
                 currentString = regex.Replace(currentString, target);
-                buffer.Append(currentString + "\n");
+                string currentString2;
                 while (currentString != null)
                 {
-                    prevString = currentString;
-                    prevString = regex.Replace(prevString, target);
-                    currentString = reader.ReadLine();
-                   
-                    if (prevString == "<NDS_v3_reestr_t001>")
+                    if (currentString.Trim() == startTag2) flag2 = true;
+                    if (currentString.Trim() == endTag2) flag2 = false;
+                    if (currentString.Trim() == startTag8) flag8 = true;
+                    if (currentString.Trim() == endTag8) flag8 = false;
+
+                    if (flag2)
                     {
-                        buffer.Append("Fuck!" + "\n");
+                        buffer.Append("Bingo2!" + "\n");
+                        try
+                        {
+                            reader2 = File.OpenText(openFileDialog2.FileName);
+                            currentString2 = reader2.ReadLine();
+                            currentString2 = regex.Replace(currentString2, target);
+                            var flagCur = false;
+                            while (currentString2 != null)
+                            {
+                                if (currentString2.Trim() == startTag2) flagCur = true;
+                                if (currentString2.Trim() == endTag2) flagCur = false;
+
+                                if (flagCur)
+                                {
+                                    buffer.Append(currentString2 + "\n");
+                                }
+
+                                currentString2 = reader2.ReadLine();
+                            }
+                            flagCur = false;
+                        }
+                        catch (IOException ee)
+                        {
+                            label1.Text = "2 " + ee.Message;
+                        }
+                        finally
+                        {
+                            if (reader2 != null)
+                                reader2.Dispose();
+                        }
+                    }
+                    else if (flag8)
+                    {
+                        buffer.Append("Bingo8!" + "\n");
+                        try
+                        {
+                            reader2 = File.OpenText(openFileDialog2.FileName);
+                            currentString2 = reader2.ReadLine();
+                            currentString2 = regex.Replace(currentString2, target);
+                            var flagCur = false;
+                            while (currentString2 != null)
+                            {
+                                if (currentString2.Trim() == startTag8) flagCur = true;
+                                if (currentString2.Trim() == endTag8) flagCur = false;
+
+                                if (flagCur)
+                                {
+                                    buffer.Append(currentString2 + "\n");
+                                }
+
+                                currentString2 = reader2.ReadLine();
+                            }
+                            flagCur = false;
+                        }
+                        catch (IOException ee)
+                        {
+                            label1.Text = "8 " + ee.Message;
+                        }
+                        finally
+                        {
+                            if (reader2 != null)
+                                reader2.Dispose();
+                        }
                     }
                     else buffer.Append(currentString + "\n");
-                    
+                    label2.Text = currentString;
+                    currentString = reader.ReadLine();
                 }
                 richTextBox1.Text = buffer.ToString();
                 buffer.Clear();
@@ -73,7 +148,7 @@ namespace XMLApplication
             catch (IOException ee)
             {
                 // Console.WriteLine("Error reading. {0}", ee.Message);
-                label1.Text = ee.Message;
+                label1.Text = "Main " + ee.Message;
             }
             finally
             {
