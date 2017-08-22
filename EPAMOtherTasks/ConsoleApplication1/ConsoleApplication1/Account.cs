fc3e0e44a80d6b781a685ed0c8538b476e0be96d
dmitri-desh <dmitri.desh@gmail.com>
2017-08-22 09:54:28 +0300

    Init Application

---
+++ EPAMOtherTasks/ConsoleApplication1/ConsoleApplication1/Account.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Account
+    {
+        int _sum; // Переменная для хранения суммы
+        int _percentage; // Переменная для хранения процента
+
+        // Объявляем делегат
+        public delegate void AccountStateHandler(string message);
+        // Создаем переменную делегата
+        AccountStateHandler del;
+        // Регистрируем делегат
+        // Регистрируем делегат
+        public void RegisterHandler(AccountStateHandler _del)
+        {
+            del += _del; // добавляем делегат
+        }
+        // Отмена регистрации делегата
+        public void UnregisterHandler(AccountStateHandler _del)
+        {
+            del -= _del; // удаляем делегат
+        }
+
+        public Account(int sum, int percentage)
+        {
+            _sum = sum;
+            _percentage = percentage;
+        }
+
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum <= _sum)
+            {
+                _sum -= sum;
+
+                if (del != null)
+                    del("Сумма " + sum.ToString() + " снята со счета");
+            }
+            else
+            {
+                if (del != null)
+                    del("Недостаточно денег на счете");
+            }
+        }
+
+        public int Percentage
+        {
+            get { return _percentage; }
+        }
+    }
+}
---
+++ EPAMOtherTasks/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(200, 6);
+            Account.AccountStateHandler colorDelegate = new Account.AccountStateHandler(Color_Message);
+
+            // Добавляем в делегат ссылку на методы
+            account.RegisterHandler(new Account.AccountStateHandler(Show_Message));
+            account.RegisterHandler(colorDelegate);
+            // Два раза подряд пытаемся снять деньги
+            account.Withdraw(100);
+            account.Withdraw(150);
+
+            // Удаляем делегат
+            account.UnregisterHandler(colorDelegate);
+            account.Withdraw(50);
+
+            Console.ReadLine();
+        }
+        private static void Show_Message(String message)
+        {
+            Console.WriteLine(message);
+        }
+        private static void Color_Message(string message)
+        {
+            // Устанавливаем красный цвет символов
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            // Сбрасываем настройки цвета
+            Console.ResetColor();
+        }
+    }
+}
