da4e4cad83ce7d1a8ee99943f79339a76da18f27
dmitri-desh <dmitri.desh@gmail.com>
2017-08-22 09:52:37 +0300

    Init Application

---
+++ EPAMOtherTasks/ItAcad/ItAcad/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItAcad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 23;
+            int k = 3;
+            // формирую массив
+            List<int> arrRand = new List<int>();
+            Random rand = new Random((int)Stopwatch.GetTimestamp());
+            for (int i = 0; i < k; i++)
+                arrRand.Add(rand.Next());
+            for (int i = k; i < n; i = i + 2)
+            {
+                arrRand.Add(rand.Next());
+                arrRand.Add(arrRand[i]);
+            }
+            var sorted = arrRand.OrderBy(a => Guid.NewGuid()).ToList();
+            arrRand.Clear();
+            arrRand.AddRange(sorted);
+            int[] arr = arrRand.ToArray();
+            for (int i = 0; i < n; i++)
+                 Console.Write("{0} ", arr[i]);
+        // массив сформирован
+
+        }
+    }
+}
